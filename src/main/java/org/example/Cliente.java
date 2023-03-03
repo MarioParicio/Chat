f25d4b04792cf3dd4bdf6d18044c9612fbf1d436
Mario Paricio <mpariciob@gmail.com>
2023-03-03 21:19:21 +0100

    Initial commit

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Cliente implements Runnable {
+
+    // Incluimos la dirección del servidor
+    private String host;
+    private int puerto;
+    private String mensaje;
+
+    //public Cliente(int puerto, String mensaje) {
+    public Cliente(String host, int puerto, String mensaje) {
+        this.host= host;
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public void run() {
+        //Host del servidor
+        // Elimino la dirección del servido como constante.
+      //  final String HOST = "127.0.0.1";
+        //Puerto del servidor
+        DataOutputStream out;
+
+        try {
+            //Creo el socket para conectarme con el cliente
+           // Socket sc = new Socket(HOST, puerto);
+           //Creo el socket para conectarme con el cliente con la dirección del servidor.
+            Socket sc = new Socket(host, puerto);
+
+            out = new DataOutputStream(sc.getOutputStream());
+
+            //Envio un mensaje al cliente
+            out.writeUTF(mensaje);
+
+            sc.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
